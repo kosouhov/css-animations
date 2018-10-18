d931e0f9297802bd0225b0185bf6ff7fe54c9ae8
kosouhov <vladimir@kosouhov.com>
2018-10-18 08:47:27 +0300

    add pictures, add script for retina-ready

---
+++ layout/script.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+	let images = document.querySelectorAll('.retina-img')
+	for (var i = 0; i < images.length; i++) {
+		let e = images[i]
+		e.style.width = e.width / 2 + 'px'
+	}
+}
